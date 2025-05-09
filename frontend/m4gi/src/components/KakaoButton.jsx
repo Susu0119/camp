f3b396198beee639870d160d7e911b53a8864756
m4gicoras <m4gicoras@gmail.com>
2025-05-09 13:02:30 +0900

    KakaoButton 컴포넌트 추가
    <KakaoButton /> : '로그인' 텍스트 버튼
    
    <KakaoButton variant="short" /> : '카카오 로그인' 텍스트 짧은 버튼
    
    <KakaoButton variant="long" /> : '카카오 로그인' 텍스트 긴 버튼

---
+++ frontend/m4gi/src/components/KakaoButton.jsx
@@ -0,0 +1,36 @@
+// components/ui/KakaoButton.jsx
+import React from 'react';
+
+const VARIANTS = {
+    cont: {
+        image: 'https://developers.kakao.com/tool/resource/static/img/button/login/simple/ko/kakao_login_large.png',
+        width: 'w-[80px]',
+    },
+    short: {
+        image: 'https://developers.kakao.com/tool/resource/static/img/button/login/full/ko/kakao_login_large_narrow.png',
+        width: 'w-[160px]',
+    },
+    long: {
+        image: 'https://developers.kakao.com/tool/resource/static/img/button/login/full/ko/kakao_login_large_wide.png',
+        width: 'w-[260px]',
+    },
+};
+
+export function KakaoButton({
+    variant = 'cont',       // 'cont' | 'short' | 'long'
+    image,                  // 직접 URL을 덮어쓰고 싶으면 전달
+    alt = '카카오 로그인',
+    className = '',
+    ...props
+}) {
+    const { image: src, width } = VARIANTS[variant] || VARIANTS.cont;
+
+    return (
+        <button
+            {...props}
+            className={`select-none cursor-pointer m-4 border-none bg-transparent ${width} ${className}`}
+        >
+            <img src={src} alt={alt} className="block" />
+        </button>
+    );
+}
