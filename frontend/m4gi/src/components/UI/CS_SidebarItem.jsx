8a6480ddeec7edfc59e6abd780d3cf95f9adde35
m4gicoras <m4gicoras@gmail.com>
2025-05-12 12:47:53 +0900

    고객센터 관련 컴포넌트 추가
    CS_Main : 고객센터 메인 화면
    CS_Header : 고객센터 헤더
    CS_MenuButton : 고객센터 버튼
    CS_Siderbar : 고객센터 사이드 바
    CS_SidebarItem : 고객센터 사이드 바 내용용

---
+++ frontend/m4gi/src/components/UI/CS_SidebarItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function SidebarItem({ text, onClick, isCategory = false }) {
+    if (isCategory) {
+        return (
+            <div className="my-2 px-4">
+                <div className="flex items-center">
+                    <span className="text-sm font-normal text-[#71717a]">
+                        {text}
+                    </span>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div
+            className="flex items-center px-4 py-2.5 rounded-md cursor-pointer hover:bg-[#f4f4f5]"
+            onClick={onClick}
+        >
+            <div className="w-4 h-4 mr-2"></div>
+            <div className="flex items-center">
+                <span className="text-sm font-bold text-black">
+                    {text}
+                </span>
+            </div>
+        </div>
+    );
+};
