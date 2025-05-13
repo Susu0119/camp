a5600e7e71359c33e4c835204ba1ccbd95790eb7
reder-dev <reder78901@gmail.com>
2025-05-13 18:17:00 +0900

    로그인 페이지 구현(카카오톡 로그인페이지 d이동동까지)

--- frontend/m4gi/src/App.jsx
+++ frontend/m4gi/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import CustomerSupport from './components/CS/CS_Main';
+import LoginPage from './components/CS/LoginPage';
+
 
 function App() {
   return (
     <div className="App">
-      <CustomerSupport />
+      <LoginPage />   {/*  LoginPage만 보이게 */}
+      {/* <CustomerSupport /> */} {/*  잠깐 주석 처리 */}
     </div>
-  )
+  );
 }
+
 
 export default App;
---
+++ frontend/m4gi/src/components/UI/LoginLogo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function LoginLogo() {
+  return (
+    <header className="flex relative gap-1 self-center max-w-full text-6xl whitespace-nowrap w-[351px] max-md:text-4xl">
+      <img
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/ad4d10228d0c6f1b0e8c1d8439f170e2c8151c78?placeholderIfAbsent=true&apiKey=e63d00b6fe174365bf8642989b3e5edd"
+        alt="Campia Logo"
+        className="object-contain shrink-0 rounded-none aspect-[1.09] w-[98px]"
+      />
+      <h1 className="flex-auto w-[230px] text-white max-md:text-4xl">
+        Campia
+      </h1>
+    </header>
+  );
+}
