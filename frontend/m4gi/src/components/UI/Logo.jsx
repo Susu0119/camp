e5640b08feef7b20386de750200e9b65e2d2b838
m4gicoras <m4gicoras@gmail.com>
2025-05-14 18:07:40 +0900

    예시 코드 제거

--- frontend/m4gi/src/components/UI/Logo.jsx
+++ frontend/m4gi/src/components/UI/Logo.jsx
@@ -3,12 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default function Logo() {
     return (
-        <Link to="/starry">
-            <div className="flex select-none items-center justify-center">
-                <span className="font-['Gapyeong_Wave'] text-3xl font-normal leading-[44px] text-[#6e0488]">
-                    Campia
-                </span>
-            </div>
-        </Link>
+        <div className="flex select-none items-center justify-center">
+            <span className="font-['Gapyeong_Wave'] text-3xl font-normal leading-[44px] text-[#6e0488]">
+                Campia
+            </span>
+        </div>
     );
 };
