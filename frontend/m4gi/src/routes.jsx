2497efe193c4cc710892ce6e965b345b4f771b10
reder-dev <reder78901@gmail.com>
2025-05-14 18:04:57 +0900

    login 페이지 등록록

--- frontend/m4gi/src/routes.jsx
+++ frontend/m4gi/src/routes.jsx
@@ -2,12 +2,14 @@
 import CS_Main from './components/CS/CS_Main';
 import AdminReservationList from './components/Admin/AdminReservationList';
 import CustomerSupport from './components/CS/CS_Main';
+import { LoginPage } from './components/CS/LoginPage';
 const routeList = [
   
   { path: '/test/admin-reservation', element: <AdminReservationList /> },
   { path: '/admin', element: <AdminReservationList /> },
   { path: '/test/cs', element: <CS_Main /> },
   { path: '/', element: <CS_Main /> },
+  { path: '/login', element: <LoginPage /> },
 
   // 404 fallback
   { path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
