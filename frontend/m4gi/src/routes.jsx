0e88d2c6c753d9c975a451b10ba636cc708dbba8
yemyum <oyl582@naver.com>
2025-05-14 16:11:16 +0900

    routes.jsx : 라우팅 관리 컴포넌트 추가
    / : 첫 페이지
    * : 에러 페이지

---
+++ frontend/m4gi/src/routes.jsx
@@ -0,0 +1,16 @@
+// src/routes.js
+import CS_Main from './components/CS/CS_Main';
+import AdminReservationList from './components/Admin/AdminReservationList';
+import CustomerSupport from './components/CS/CS_Main';
+const routeList = [
+  
+  { path: '/test/admin-reservation', element: <AdminReservationList /> },
+  { path: '/admin', element: <AdminReservationList /> },
+  { path: '/test/cs', element: <CS_Main /> },
+  { path: '/', element: <CS_Main /> },
+
+  // 404 fallback
+  { path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
+];
+
+export default routeList;
