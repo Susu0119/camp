b5ac1350ff48c3da2b0df3b073912001cd5b6ce2
reder-dev <reder78901@gmail.com>
2025-05-15 18:28:54 +0900

    메인맞추기

--- frontend/m4gi/src/routes.jsx
+++ frontend/m4gi/src/routes.jsx
@@ -1,28 +1,22 @@
 // src/routes.js
 import CS_Main from './components/CS/CS_Main';
 import AdminReservationList from './components/Admin/AdminReservationList';
-<<<<<<< HEAD
 import CustomerSupport from './components/CS/CS_Main';
 import { LoginPage } from './components/CS/LoginPage';
 import FindAccount from './components/CS/FindAccount';
-=======
 import CampingSiteCard from './components/Main/Card';
->>>>>>> origin/dev
-const routeList = [
+import MainPage from './components/Main/Main';
 
+const routeList = [
   { path: '/test/admin-reservation', element: <AdminReservationList /> },
   { path: '/admin', element: <AdminReservationList /> },
   { path: '/test/cs', element: <CS_Main /> },
-<<<<<<< HEAD
-  { path: '/', element: <CS_Main /> },
+  { path: '/', element: <MainPage /> }, // 또는 CampingSiteCard로 변경 가능
   { path: '/login', element: <LoginPage /> },
   { path: '/find', element: <FindAccount /> },
-=======
-  //{ path: '/', element: <CampingSiteCard /> },
->>>>>>> origin/dev
 
   // 404 fallback
-  //{ path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
+  // { path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
 ];
 
 export default routeList;
