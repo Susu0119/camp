e60b24b4afbd03b2c3c96f56d0e0a54cc6bdca9b
reder-dev <reder78901@gmail.com>
2025-05-15 18:09:04 +0900

    WIP: routes 및 기타 로그인 관련 작업 중

--- frontend/m4gi/src/routes.jsx
+++ frontend/m4gi/src/routes.jsx
@@ -3,6 +3,7 @@ import CS_Main from './components/CS/CS_Main';
 import AdminReservationList from './components/Admin/AdminReservationList';
 import CustomerSupport from './components/CS/CS_Main';
 import { LoginPage } from './components/CS/LoginPage';
+import FindAccount from './components/CS/FindAccount';
 const routeList = [
   
   { path: '/test/admin-reservation', element: <AdminReservationList /> },
@@ -10,6 +11,7 @@ const routeList = [
   { path: '/test/cs', element: <CS_Main /> },
   { path: '/', element: <CS_Main /> },
   { path: '/login', element: <LoginPage /> },
+  { path: '/find', element: <FindAccount /> },
 
   // 404 fallback
   { path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
