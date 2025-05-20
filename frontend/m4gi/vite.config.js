c0ed0e0ad99b186caa90ca4b665a8e38e5784633
m4gicoras <m4gicoras@gmail.com>
2025-05-20 15:01:13 +0900

    Update vite.config.js

--- frontend/m4gi/vite.config.js
+++ frontend/m4gi/vite.config.js
@@ -19,6 +19,11 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
+
+      '/web': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     }
   }
 });
