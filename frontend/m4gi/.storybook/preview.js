62ba33d63c8df5d3c81701d96a96811b03e7cf4a
m4gicoras <m4gicoras@gmail.com>
2025-05-21 23:41:21 +0900

    storybook 속성 수정

--- frontend/m4gi/.storybook/preview.js
+++ frontend/m4gi/.storybook/preview.js
@@ -1,10 +1,12 @@
 /** @type { import('@storybook/react').Preview } */
+import '../src/index.css';
+import '../src/App.css';
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
