323893b9b55f7900f7ed3d5efa5c4f087a066b2a
m4gicoras <m4gicoras@gmail.com>
2025-05-22 14:34:38 +0900

    Merge

--- frontend/m4gi/src/routes.jsx
+++ frontend/m4gi/src/routes.jsx
@@ -1,10 +1,7 @@
-
-
 const routeList = [
 
-
-  // 404 fallback
-  // { path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
+  //404 fallback
+  { path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
 ];
 
 export default routeList;
