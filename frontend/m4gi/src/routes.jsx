4939cc935b7cddad6b8f49732a6f9f2ac7a2ce8e
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-05-23 12:54:39 +0900

    routes.jsx 에 CampSearchPage 경로 추가 (임시)

--- frontend/m4gi/src/routes.jsx
+++ frontend/m4gi/src/routes.jsx
@@ -1,5 +1,7 @@
-const routeList = [
+import CampSearchPage from './pages/Main/Main_CampSearchPage'
 
+const routeList = [
+  { path: '/search', element: <CampSearchPage /> },
   //404 fallback
   { path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
 ];
