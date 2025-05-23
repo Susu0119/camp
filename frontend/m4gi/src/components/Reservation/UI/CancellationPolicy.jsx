c2a0f826dab6bd953a2b40e814a88338bc8cf02d
reder-dev <reder78901@gmail.com>
2025-05-23 10:52:20 +0900

    test3

--- frontend/m4gi/src/components/Reservation/UI/CancellationPolicy.jsx
+++ frontend/m4gi/src/components/Reservation/UI/CancellationPolicy.jsx
@@ -6,7 +6,7 @@ const CancellationPolicy = () => {
       <h2 className="px-0 py-0.5 mt-6 text-lg font-bold text-fuchsia-700 h-[26px] w-[1250px] max-md:w-full">
         취소 및 환불 정책
       </h2>
-      <section className="flex flex-col gap-1 items-start px-4 pt-4 pb-5 bg-purple-200 rounded-md h-[105px] w-[1250px] max-md:w-full">
+      <section className="flex flex-col gap-1 items-start px-4 pt-4 pb-5 bg-purple-200 rounded-md h-[120px] w-[1250px] max-md:w-full">
         <div className="text-base text-zinc-800 max-sm:text-sm">
           <p>환불 정책</p>
           <p>14일 전 : 전액 환불</p>
--- frontend/m4gi/src/routes.jsx
+++ frontend/m4gi/src/routes.jsx
@@ -1,4 +1,8 @@
+import ReservationPage from "./pages/Reservation/ReservationPage";
+
 const routeList = [
+
+  { path: '/reservation', element: <ReservationPage /> },
 
   //404 fallback
   { path: '*', element: <h1>404 - 페이지를 찾을 수 없습니다</h1> },
