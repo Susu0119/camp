fc53083e47cdaf2bf0cd2a4a4138916478dbce5c
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-05-23 23:31:23 +0900

    헤더 검색창 제거 (showSearchBar - false 추가)

--- frontend/m4gi/src/pages/Main/Main_CampSearchPage.jsx
+++ frontend/m4gi/src/pages/Main/Main_CampSearchPage.jsx
@@ -6,7 +6,7 @@ export default function MainCampSearchPage() {
   return (
     <main className="overflow-hidden bg-white">
       <div className="flex-1 w-full max-md:max-w-full">
-        <Header />
+        <Header showSearchBar={false} />
         <section className="flex-1 px-20 pt-2.5 w-full max-md:px-5 max-md:max-w-full">
           <SearchForm />
         </section>
