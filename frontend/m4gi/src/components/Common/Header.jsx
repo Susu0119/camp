4b7efe59754d13549fee7be970523e20c59bd8ba
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-05-25 21:57:04 +0900

    Header의 캠피아 로고 색 cpurple로 변경

--- frontend/m4gi/src/components/Common/Header.jsx
+++ frontend/m4gi/src/components/Common/Header.jsx
@@ -8,13 +8,13 @@ export default function Header({ showSearchBar = true }) {
         : "flex gap-10 justify-between px-12 w-full bg-white min-h-[100px] shadow-[0px_2px_4px_rgba(0,0,0,0.1)]"
     return (
         <header className={header}>
-            <div className="flex gap-2.5 select-none font-['Gapyeong_Wave'] items-center self-stretch my-auto text-4xl text-fuchsia-700 whitespace-nowrap">
+            <div className="flex gap-2.5 select-none font-['Gapyeong_Wave'] items-center self-stretch my-auto text-4xl text-cpurple whitespace-nowrap">
                 <img
                     src="https://cdn.builder.io/api/v1/image/assets/2e85db91f5bc4c1490f4944382f6bff3/ac1e1903cdffe41cf50fd0a5d741c49309973b46?placeholderIfAbsent=true"
                     alt="Campia Logo"
                     className="object-contain shrink-0 self-stretch my-auto rounded-none aspect-[1.09] w-[59px]"
                 />
-                <h1 className="self-stretch my-auto text-fuchsia-700">Campia</h1>
+                <h1 className="self-stretch my-auto text-cpurple">Campia</h1>
             </div>
 
             {/* showSearchBar prop이 true일 때만 SearchBar를 렌더링합니다. */}
