3a357f1528cef0cc56f3e93504fc585d1e23fcb2
reder-dev <reder78901@gmail.com>
2025-05-30 16:42:27 +0900

    회원 탈퇴 백엔드 완료

--- workspace_project/Project/src/main/java/com/m4gi/util/GoogleAuthorizationUtil.java
+++ workspace_project/Project/src/main/java/com/m4gi/util/GoogleAuthorizationUtil.java
@@ -9,65 +9,41 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
 
-import java.io.File;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
-import java.nio.channels.Channels;
+import java.io.InputStream;
+import java.io.File;
 import java.util.Collections;
 import java.util.List;
 
 public class GoogleAuthorizationUtil {
-    private static final String PROJECT_ID        = "latale-1d43a";
-    private static final String BUCKET_NAME       = "m4gi";
-    private static final String CLIENT_JSON_OBJECT = "client.json";
-    // Gmail scope
-    private static final List<String> SCOPES =
-            Collections.singletonList("https://www.googleapis.com/auth/gmail.send");
+
+    private static final List<String> SCOPES = Collections.singletonList("https://www.googleapis.com/auth/gmail.send");
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static final String TOKENS_DIR = "tokens";
 
+    public static Credential getCredential(NetHttpTransport HTTP_TRANSPORT) throws Exception {
 
-
-    public static Credential getCredential(NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // 1) GCS 클라이언트 생성
-        Storage storage = StorageOptions.newBuilder()
-                .setProjectId(PROJECT_ID)
-                .build()
-                .getService();
-
-        // 2) client.json Blob 가져오기
-        Blob blob = storage.get(BlobId.of(BUCKET_NAME, CLIENT_JSON_OBJECT));
-        if (blob == null) {
-            throw new IOException("GCS에 client.json이 없습니다: gs://"
-                    + BUCKET_NAME + "/" + CLIENT_JSON_OBJECT);
+        // ✅ resources/client.json 불러오기
+        InputStream in = GoogleAuthorizationUtil.class.getClassLoader().getResourceAsStream("client.json");
+        if (in == null) {
+            throw new IllegalArgumentException("client.json not found in resources/");
         }
 
-        // 3) InputStreamReader로 파싱
-        try (InputStream in = Channels.newInputStream(blob.reader());
-             InputStreamReader reader = new InputStreamReader(in)) {
-            GoogleClientSecrets clientSecrets =
-                    GoogleClientSecrets.load(JSON_FACTORY, reader);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-            // 4) 인증 흐름 구축
-            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                    .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIR)))
-                    .setAccessType("offline")
-                    //.setApprovalPrompt("force")
-                    .build();
+        // ✅ 인증 흐름 생성
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIR)))
+                .setAccessType("offline")
+                .build();
 
-            LocalServerReceiver receiver = new LocalServerReceiver.Builder()
-                    .setPort(8888)
-                    .build();
+        // ✅ 인증 콜백 포트 8889
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder()
+                .setPort(8889)
+                .build();
 
-            return new AuthorizationCodeInstalledApp(flow, receiver)
-                    .authorize("user");
-        }
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 }
