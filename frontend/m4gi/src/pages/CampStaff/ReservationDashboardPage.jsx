8d3d133212f493e1138b7f40a132a952658eef51
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-06-01 12:30:23 +0900

    컴포넌트 파일 경로 변경

--- frontend/m4gi/src/pages/CampStaff/ReservationDashboardPage.jsx
+++ frontend/m4gi/src/pages/CampStaff/ReservationDashboardPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../components/Common/Header";
-import Sidebar from "../../components/MyPage/MP_SideBar";
-import ReservationList from "../../components/CampStaff/ReservationList";
+import Sidebar from "../../components/MyPage/UI/MP_SideBar";
+import ReservationList from "../../components/CampStaff/UI/ReservationList";
 
 export default function ReservationDashboard () {
   return (
