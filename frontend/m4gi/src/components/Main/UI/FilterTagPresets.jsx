edd8a8953efa24b7b34ea9c2e6a336de416906da
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-06-04 17:53:40 +0900

    feat: 필터 태그 사용위해 라벨, 필터 파일 추가

---
+++ frontend/m4gi/src/components/Main/UI/FilterTagPresets.jsx
@@ -0,0 +1,37 @@
+export const tagPresets = [
+    {
+        label: "감성 가득, 별빛 글램핑",
+        filters: {
+            featureList: ["NIGHT_VIEW_SPOT"],
+            campgroundTypes: ["GLAMPING"],
+        }
+    },
+    {
+        label: "여름방학, 아이들과 함께",
+        filters: {
+            // featureList: ["KIDS_ALLOWED", "WATER_ACTIVITIES", "PRIVATE_SHOWER", "WIFI", "CONVENIENCE_STORE"],
+            featureList: ["KIDS_ALLOWED", "WATER_ACTIVITIES", "PRIVATE_SHOWER"], // 조건 완화
+        }
+    },
+    {
+        label: "댕댕이와 여름 휴가",
+        filters: {
+            featureList: ["PET_ALLOWED", "WATER_ACTIVITIES"],
+        }
+    },
+    {
+        label: "오션뷰 감성 차박",
+        filters: {
+            featureList: ["SEA", "CAMPFIRE_ALLOWED"],
+            campgroundTypes: ["AUTO"],
+        }
+    },
+    {
+        label: "도심 속 가벼운 감성 캠프닉",
+        filters: {
+            featureList: ["CITY_VIEW", "PUBLIC_BBQ"],
+            campgroundTypes: ["GLAMPING"],
+        }
+    }
+
+];
