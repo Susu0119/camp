d8903a40b42569e3cc312c10ed38138a0153aff4
j5289 <goldcat2005@naver.com>
2025-06-05 14:45:22 +0900

    마이페이지 메인 - 로그인한 사용자 닉네임 및 프로필 사진 출력, 환영문구 출력

--- frontend/m4gi/src/pages/MyPage/MyPage_Main.jsx
+++ frontend/m4gi/src/pages/MyPage/MyPage_Main.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { WelcomeSection } from "../../components/MyPage/UI/MP_Main_WelcomSection";
 import CSSidebar from "../../components/MyPage/UI/MP_SideBar";
 import MPHeader from "../../components/MyPage/UI/MP_Header";
+import axios from "axios";
 
 export default function MyPageMain() {
+  const [userData, setUserData] = useState({
+    nickname: "",
+    profileImage: "/images/default-profile.jpg", // 기본 이미지
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/web/api/user/mypage/mypage/main", { withCredentials: true })
+      .then((response) => {
+        const { nickname, profileImage } = response.data;
+        setUserData({
+          nickname,
+          profileImage: profileImage || "/images/default-profile.jpg",
+        });
+      })
+      .catch((error) => {
+        console.error("마이페이지 정보 불러오기 실패:", error);
+      });
+  }, []);
+
   return (
     <div className="h-screen flex flex-col">
       <MPHeader />
@@ -11,8 +32,8 @@ export default function MyPageMain() {
         <CSSidebar />
         <div className="flex-1">
           <WelcomeSection
-            nickname="홍길동"
-            profileImage="/images/default-profile.jpg"
+            nickname={userData.nickname}
+            profileImage={userData.profileImage}
           />
         </div>
       </div>
