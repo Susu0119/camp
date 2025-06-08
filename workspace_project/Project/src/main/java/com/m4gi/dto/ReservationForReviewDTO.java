a44b0c6fa5031dd75d0bca1745ef334d02acb244
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-06-08 13:26:55 +0900

    feat: 리뷰 작성 대상 선택에 캠핑장/사이트명 표시 - 리뷰 작성 가능 목록을 조회하는 MyBatis 매퍼 쿼리에 캠핑장(campgrounds) 및 사이트(campground_sites) 테이블을 JOIN하여 캠핑장명과 사이트명을 함께 가져오도록 추가 - 장소 선택 드롭다운에 '캠리버사이드 - 오토캠핑 A-1'과 같이 표시

--- workspace_project/Project/src/main/java/com/m4gi/dto/ReservationForReviewDTO.java
+++ workspace_project/Project/src/main/java/com/m4gi/dto/ReservationForReviewDTO.java
@@ -8,9 +8,9 @@ import lombok.Data;
 @Data
 public class ReservationForReviewDTO {
 	private String reservationId;
-	private int campgroundId;
+	private String campgroundId;
 	private String campgroundName; // 이용 장소 (캠핑장명)
-	private String reservationSite; // 이용 장소 (사이트명)
+	private String siteName; // 이용 장소 (사이트명)
 	private LocalDate reservationDate; // 이용 날짜
 	private LocalDate endDate; // 종료 날짜
 }
