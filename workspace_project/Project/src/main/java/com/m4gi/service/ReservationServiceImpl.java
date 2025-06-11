70e9bea2b9f25546995115a1cbc338fcc688527c
m4gicoras <m4gicoras@gmail.com>
2025-06-11 12:50:13 +0900

    import 누락 수정

--- workspace_project/Project/src/main/java/com/m4gi/service/ReservationServiceImpl.java
+++ workspace_project/Project/src/main/java/com/m4gi/service/ReservationServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.m4gi.dto.ReservationDTO;
 import com.m4gi.mapper.ReservationMapper;
+import java.util.List;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
@@ -16,7 +17,7 @@ public class ReservationServiceImpl implements ReservationService {
     public ReservationDTO findReservationById(String reservationId) {
         System.out.println("DB에서 예약 정보 조회 시도 - reservationId: " + reservationId);
         ReservationDTO reservation = reservationMapper.findById(reservationId);
-        if(reservation == null) {
+        if (reservation == null) {
             System.out.println("예약 정보를 DB에서 찾을 수 없습니다. ID: " + reservationId);
         } else {
             System.out.println("예약 정보 조회 성공: " + reservation.getReservationSite());
@@ -28,6 +29,5 @@ public class ReservationServiceImpl implements ReservationService {
     public List<ReservationDTO> getReservationsByProvider(Integer providerCode, String providerUserId) {
         return reservationMapper.findByProvider(providerCode, providerUserId);
     }
-
 
 }
