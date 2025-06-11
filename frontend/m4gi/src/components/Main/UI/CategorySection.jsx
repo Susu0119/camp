60f17fb53fe605fe2b8429d362ba8190464a7a22
Susu0119 <g2310077@gl.aiu.ac.jp>
2025-06-11 11:03:07 +0900

    feat: 메인페이지 상단 CategorySection 찜 제외 이동 가능하도록 수정 (필터링 기능)

--- frontend/m4gi/src/components/Main/UI/CategorySection.jsx
+++ frontend/m4gi/src/components/Main/UI/CategorySection.jsx
@@ -1,46 +1,23 @@
-export default function CategorySection() {
+export default function CategorySection({ onCategoryClick  }) {
     // Array of category items
     const categories = [
-        {
-            name: "캠핑",
-            image: 'https://storage.googleapis.com/m4gi/images/1.svg' // 수정된 링크
-        },
-        {
-            name: "글램핑",
-            image: 'https://storage.googleapis.com/m4gi/images/2.svg'
-        },
-        {
-            name: "카라반",
-            image: 'https://storage.googleapis.com/m4gi/images/3.svg'
-        },
-        {
-            name: "차박",
-            image: 'https://storage.googleapis.com/m4gi/images/4.svg'
-        },
-        {
-            name: "반려동물",
-            image: 'https://storage.googleapis.com/m4gi/images/5.svg'
-        },
-        {
-            name: "키즈",
-            image: 'https://storage.googleapis.com/m4gi/images/6.svg'
-        },
-        {
-            name: "캠프닉",
-            image: 'https://storage.googleapis.com/m4gi/images/7.svg'
-        },
-        {
-            name: "찜",
-            image: 'https://storage.googleapis.com/m4gi/images/8.svg'
-        }
-    ]
+        { name: "캠핑",     value: "CAMPING",       image: 'https://storage.googleapis.com/m4gi/images/1.svg' },
+        { name: "글램핑",   value: "GLAMPING",      image: 'https://storage.googleapis.com/m4gi/images/2.svg' },
+        { name: "카라반",   value: "CARAVAN",       image: 'https://storage.googleapis.com/m4gi/images/3.svg' },
+        { name: "차박",     value: "AUTO",          image: 'https://storage.googleapis.com/m4gi/images/4.svg' }, // 차박을 오토캠핑(AUTO)으로 가정
+        { name: "반려동물", value: "PET_ALLOWED",   image: 'https://storage.googleapis.com/m4gi/images/5.svg' },
+        { name: "키즈",     value: "KIDS_ALLOWED",  image: 'https://storage.googleapis.com/m4gi/images/6.svg' },
+        { name: "캠프닉",   value: "CAMPNIC",       image: 'https://storage.googleapis.com/m4gi/images/7.svg' },
+        { name: "찜",       value: "WISHLIST",      image: 'https://storage.googleapis.com/m4gi/images/8.svg' }
+    ];
 
     return (
         <section className="flex overflow-hidden gap-10 justify-center items-center text-xl font-bold leading-none text-center text-black whitespace-nowrap h-[85px] w-[1322px]">
             {categories.map((category, index) => (
                 <div
                     key={index}
-                    className="flex overflow-hidden gap-2 items-center flex-col grow shrink self-stretch px-1 my-auto bg-white bg-opacity-0 min-h-[103px] w-[74px]"
+                    onClick={() => onCategoryClick(category.value)}
+                    className="flex overflow-hidden cursor-pointer gap-2 items-center flex-col grow shrink self-stretch px-1 my-auto bg-white bg-opacity-0 min-h-[103px] w-[74px]"
                 >
                     <img
                         src={category.image}
